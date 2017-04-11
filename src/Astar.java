8c97f7f1cc6a80728f44eaa443596cb861a1d9e2
pavithB <pavithbuddhima@gmail.com>
2017-04-12 03:47:40 +0530

    commenting ( date* 2017/ 4/ 23 )

--- src/Astar.java
+++ src/Astar.java
@@ -200,6 +200,8 @@ public class Astar {
             // Left
             if (current.getJ() - 1 >= 0) {
                 temp = nodeGrid[current.getI()][current.getJ() - 1];
+                //check that temp node is already visited , is it a blocked node or its old f value is less than new f value if it is skip that node
+                //if it's not visited , not blocked or old f value not less than new f value then check and add that node to openlist
                 if (!temp.visited && !temp.blocked && temp.getfValue() > current.getfValue() + normalDistance) {
 
 
